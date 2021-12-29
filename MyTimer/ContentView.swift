7b3682ffc55090f4d9934ea2b0cbea9ea9e68413
shoishizaki <shoishizaki1997@gmail.com>
2021-12-29 16:31:06 +0900

    Create MyTimer

--- MyTimer/ContentView.swift
+++ MyTimer/ContentView.swift
@@ -1,16 +1,92 @@
-//
-//  ContentView.swift
-//  MyTimer
-//
-//  Created by 石崎將 on 2021/12/29.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State var timerHandler : Timer?
+    
+    @State var count = 0
+    
+    @AppStorage("timer_value") var timerValue = 10
+    
+    @State var showAlert = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+                Image("backgroundTimer")
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                    .aspectRatio(contentMode: .fill)
+                
+                VStack(spacing: 30.0) {
+                    Text("残り\(timerValue - count)秒")
+                        .font(.largeTitle)
+                    
+                    HStack {
+                        Button(action: {
+                            startTimer()
+                        }) {
+                            Text("スタート")
+                                .font(.title)
+                                .foregroundColor(Color.white)
+                                .frame(width: 140, height: 140).background(Color("startColor"))
+                                .clipShape(Circle())
+                        }
+                        
+                        Button(action: {
+                            if let unwrapedTimerHandler = timerHandler {
+                                if unwrapedTimerHandler.isValid == true {
+                                    unwrapedTimerHandler.invalidate()
+                                }
+                            }
+                        }) {
+                            Text("ストップ")
+                                .font(.title)
+                                .foregroundColor(Color.white)
+                                .frame(width: 140, height: 140).background(Color("stopColor"))
+                                .clipShape(Circle())
+                        }
+                    }
+                }
+            }
+            .onAppear {
+                count = 0
+            }
+            .navigationBarItems(trailing:
+                                    NavigationLink(destination: SettingView()) {
+                Text("秒数設定")
+            })
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("終了"),
+                      message: Text("タイマー終了時間です"),
+                      dismissButton: .default(Text("OK")))
+            }
+        }
+    }
+    
+    func countDownTimer() {
+        count += 1
+        
+        if timerValue - count <= 0 {
+            timerHandler?.invalidate()
+            
+            showAlert = true
+        }
+    }
+    
+    func startTimer() {
+        if let unwrapedTimerHeadler = timerHandler {
+            if unwrapedTimerHeadler.isValid == true {
+                return
+            }
+        }
+        
+        if timerValue - count <= 0 {
+            count = 0
+        }
+        
+        timerHandler = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            countDownTimer()
+        }
     }
 }
 
---
+++ MyTimer/SettingView.swift
@@ -0,0 +1,45 @@
+import SwiftUI
+
+struct SettingView: View {
+    
+    @AppStorage("timer_value") var timmerValue = 10
+    
+    var body: some View {
+        ZStack {
+            Color("backgroundSetting")
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                
+                Text("\(timmerValue)秒")
+                    .font(.largeTitle)
+                
+                Spacer()
+                
+                Picker(selection: $timmerValue, label: Text("選択")) {
+                    Text("10")
+                        .tag(10)
+                    Text("20")
+                        .tag(20)
+                    Text("30")
+                        .tag(30)
+                    Text("40")
+                        .tag(40)
+                    Text("50")
+                        .tag(50)
+                    Text("60")
+                        .tag(60)
+                }
+                
+                Spacer()
+            }
+        }
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
