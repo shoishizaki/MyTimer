075a8d4916d0a17fbbdc39669c824359cf6cc21b
shoishizaki <shoishizaki1997@gmail.com>
2021-12-29 13:18:28 +0900

    Initial Commit

---
+++ MyTimer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyTimer
+//
+//  Created by 石崎將 on 2021/12/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyTimer/MyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerApp.swift
+//  MyTimer
+//
+//  Created by 石崎將 on 2021/12/29.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
